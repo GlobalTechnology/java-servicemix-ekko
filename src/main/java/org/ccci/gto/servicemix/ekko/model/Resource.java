08de11c1c305497b2fa24b3720ecfb6a0da0d5db
Daniel Frett <daniel.frett@ccci.org>
2013-05-08 15:51:38 -0400

    added Resource model

---
+++ src/main/java/org/ccci/gto/servicemix/ekko/model/Resource.java
@@ -0,0 +1,107 @@
+package org.ccci.gto.servicemix.ekko.model;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+
+import org.apache.openjpa.persistence.jdbc.ForeignKey;
+import org.apache.openjpa.persistence.jdbc.ForeignKeyAction;
+
+@Entity
+public class Resource {
+    @EmbeddedId
+    private ResourcePrimaryKey key;
+
+    @MapsId("courseId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "courseId", referencedColumnName = "id")
+    @ForeignKey(updateAction = ForeignKeyAction.CASCADE, deleteAction = ForeignKeyAction.CASCADE)
+    private Course course;
+
+    private String mogileFsKey;
+
+    private String mimeType;
+
+    @Column(nullable = false, updatable = false)
+    private long size = 0;
+    @Column(nullable = false)
+    private boolean published = false;
+    @Column(nullable = false)
+    private boolean metaResource = false;
+
+    public Resource() {
+        this(new ResourcePrimaryKey());
+    }
+
+    public Resource(final Course course, final String sha1) {
+        this(new ResourcePrimaryKey(course, sha1));
+    }
+
+    public Resource(final Long courseId, final String sha1) {
+        this(new ResourcePrimaryKey(courseId, sha1));
+    }
+
+    public Resource(final ResourcePrimaryKey key) {
+        this.key = key;
+    }
+
+    public ResourcePrimaryKey getKey() {
+        return this.key;
+    }
+
+    public Course getCourse() {
+        return this.course;
+    }
+
+    public String getSha1() {
+        return this.key.getSha1();
+    }
+
+    public String generateMogileFsKey() {
+        return "course/" + this.key.getCourseId() + "/resource/" + this.key.getSha1();
+    }
+
+    public String getMogileFsKey() {
+        return this.mogileFsKey;
+    }
+
+    public String getMimeType() {
+        return this.mimeType;
+    }
+
+    public long getSize() {
+        return this.size;
+    }
+
+    public boolean isPublished() {
+        return this.published;
+    }
+
+    public boolean isMetaResource() {
+        return this.metaResource;
+    }
+
+    public void setMogileFsKey(final String key) {
+        this.mogileFsKey = key;
+    }
+
+    public void setMimeType(final String mimeType) {
+        this.mimeType = mimeType;
+    }
+
+    public void setSize(final long size) {
+        this.size = size;
+    }
+
+    public void setPublished(final boolean published) {
+        this.published = published;
+    }
+
+    public void setMetaResource(final boolean metaResource) {
+        this.metaResource = metaResource;
+    }
+}
---
+++ src/main/java/org/ccci/gto/servicemix/ekko/model/ResourcePrimaryKey.java
@@ -0,0 +1,57 @@
+package org.ccci.gto.servicemix.ekko.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class ResourcePrimaryKey implements Serializable {
+    private static final long serialVersionUID = -91895163112177105L;
+
+    private Long courseId = null;
+    @Column(length = 40)
+    private String sha1 = null;
+
+    public ResourcePrimaryKey() {
+    }
+
+    public ResourcePrimaryKey(final Course course, final String sha1) {
+        this(course.getId(), sha1);
+    }
+
+    protected ResourcePrimaryKey(final Long courseId, final String sha1) {
+        this.courseId = courseId;
+        if (sha1 != null) {
+            this.sha1 = sha1.toLowerCase();
+        }
+    }
+
+    public Long getCourseId() {
+        return this.courseId;
+    }
+
+    /**
+     * @return the code
+     */
+    public String getSha1() {
+        return this.sha1;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = super.hashCode();
+        hash = (hash * 31) + this.courseId.hashCode();
+        hash = (hash * 31) + this.sha1.hashCode();
+        return hash;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj == null || !(obj instanceof ResourcePrimaryKey)) {
+            return false;
+        }
+        final ResourcePrimaryKey key2 = (ResourcePrimaryKey) obj;
+        return (this.courseId.equals(key2.courseId)) && (this.sha1.equals(key2.sha1));
+    }
+}
