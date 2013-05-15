d616fae707c3fdfe70cd81ea60476482bed031b6
Daniel Frett <daniel.frett@ccci.org>
2013-05-15 11:53:38 -0400

    added uri generation logic to JaxbCourse objects

--- src/main/java/org/ccci/gto/servicemix/ekko/jaxb/model/JaxbCourse.java
+++ src/main/java/org/ccci/gto/servicemix/ekko/jaxb/model/JaxbCourse.java
@@ -1,9 +1,14 @@
 package org.ccci.gto.servicemix.ekko.jaxb.model;
 
 import static org.ccci.gto.servicemix.ekko.Constants.XMLNS_EKKO;
+import static org.ccci.gto.servicemix.ekko.jaxrs.api.Constants.PARAM_COURSE;
+import static org.ccci.gto.servicemix.ekko.jaxrs.api.Constants.PARAM_RESOURCE;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.ws.rs.core.UriBuilder;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -26,6 +31,9 @@ public class JaxbCourse {
     @XmlAttribute(name = "uri")
     private URI uri;
 
+    @XmlAttribute(name = "zipUri")
+    private URI zipUri;
+
     @XmlElement(namespace = XMLNS_EKKO, name = "meta")
     private JaxbDomElements meta;
 
@@ -40,6 +48,16 @@ public class JaxbCourse {
     }
 
     public JaxbCourse(final Course course, final boolean parseManifest) {
+        this(course, parseManifest, null, null, null);
+    }
+
+    public JaxbCourse(final Course course, final boolean parseManifest, final UriBuilder uri,
+            final Map<String, Object> uriValues) {
+        this(course, parseManifest, uri, null, uriValues);
+    }
+
+    public JaxbCourse(final Course course, final boolean parseManifest, final UriBuilder uri,
+            final UriBuilder resourceUri, final Map<String, Object> uriValues) {
         this.id = course.getId();
         this.version = course.getVersion();
         this.title = course.getTitle();
@@ -49,9 +67,27 @@ public class JaxbCourse {
             this.meta = JaxbDomElements.fromXPath(manifest, "/ekko:course/ekko:meta/*");
             this.resources = JaxbDomElements.fromXPath(manifest, "/ekko:course/ekko:resources/*");
         }
+
+        if (uri != null || resourceUri != null) {
+            final Map<String, Object> values = new HashMap<String, Object>();
+            if (uriValues != null) {
+                values.putAll(uriValues);
+            }
+
+            values.put(PARAM_COURSE, course.getId());
+
+            if (uri != null) {
+                this.uri = uri.buildFromMap(values);
+            }
+            final String zipSha1 = course.getZipSha1();
+            if (resourceUri != null && zipSha1 != null) {
+                values.put(PARAM_RESOURCE, zipSha1);
+                this.zipUri = resourceUri.buildFromMap(values);
+            }
+        }
     }
 
-    public void setUri(final URI uri) {
-        this.uri = uri;
+    public URI getUri() {
+        return uri;
     }
 }
