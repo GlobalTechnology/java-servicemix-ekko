8e463b60e7bc6be8ed793ae3519c52b1e03ba9d2
Daniel Frett <daniel.frett@ccci.org>
2013-05-17 12:02:44 -0400

    added some new functionality to the CourseManager

--- src/main/java/org/ccci/gto/servicemix/ekko/CourseManager.java
+++ src/main/java/org/ccci/gto/servicemix/ekko/CourseManager.java
@@ -1,13 +1,33 @@
 package org.ccci.gto.servicemix.ekko;
 
+import java.util.Collection;
 import java.util.List;
 
 import org.ccci.gto.servicemix.ekko.model.Course;
+import org.ccci.gto.servicemix.ekko.model.Course.CourseQuery;
+import org.ccci.gto.servicemix.ekko.model.Resource;
+import org.ccci.gto.servicemix.ekko.model.ResourcePrimaryKey;
 
 public interface CourseManager {
-    List<Course> getCourses();
-
-    List<Course> getCourses(boolean includeManifest);
+    Course createCourse(Course course);
 
     Course getCourse(Long id);
+
+    Course getCourse(CourseQuery courseQuery);
+
+    List<Course> getCourses(CourseQuery courseQuery);
+
+    Course publishCourse(CourseQuery courseQuery);
+
+    Course updateCourseAdmins(CourseQuery courseQuery, Collection<String> toAdd, Collection<String> toRemove);
+
+    Course updateCourseEnrolled(CourseQuery courseQuery, Collection<String> toAdd, Collection<String> toRemove);
+
+    Resource getResource(ResourcePrimaryKey key);
+
+    Resource storeResource(Course course, Resource resource);
+
+    void removeResource(Resource resource);
+
+    Resource storeCourseZip(Course course, Resource resource);
 }
--- src/main/java/org/ccci/gto/servicemix/ekko/CourseManagerImpl.java
+++ src/main/java/org/ccci/gto/servicemix/ekko/CourseManagerImpl.java
@@ -1,41 +1,206 @@
 package org.ccci.gto.servicemix.ekko;
 
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.persistence.EntityExistsException;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 
 import org.ccci.gto.servicemix.ekko.model.Course;
-import org.springframework.transaction.annotation.Propagation;
+import org.ccci.gto.servicemix.ekko.model.Course.CourseQuery;
+import org.ccci.gto.servicemix.ekko.model.Resource;
+import org.ccci.gto.servicemix.ekko.model.ResourcePrimaryKey;
 import org.springframework.transaction.annotation.Transactional;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 public class CourseManagerImpl implements CourseManager {
+    private static final SecureRandom RAND = new SecureRandom();
+
     @PersistenceContext
     private EntityManager em;
 
-    @Transactional(propagation = Propagation.MANDATORY)
+    @Transactional
     @Override
-    public List<Course> getCourses() {
-        return this.getCourses(false);
-    }
+    public Course createCourse(final Course course) {
+        while (true) {
+            try {
+                // generate a new id for this course
+                long id = 0;
+                while (id <= 0) {
+                    id = RAND.nextLong();
+                }
+                course.setId(id);
 
-    @Transactional(propagation = Propagation.MANDATORY)
-    @Override
-    public List<Course> getCourses(final boolean includeManifest) {
-        final CriteriaQuery<Course> cq = this.em.getCriteriaBuilder().createQuery(Course.class);
-        final Root<Course> from = cq.from(Course.class);
-        if (includeManifest) {
-            from.fetch("manifest");
+                // save this course
+                this.em.persist(course);
+            } catch (final EntityExistsException e) {
+                // a course with the specified id exists, try again
+                continue;
+            }
+
+            // we created the course successfully, break processing
+            break;
         }
-        cq.select(from);
-        return this.em.createQuery(cq).getResultList();
+
+        return course;
     }
 
-    @Transactional(propagation = Propagation.MANDATORY)
+    @Transactional
+    @Override
+    public Course publishCourse(final CourseQuery courseQuery) {
+        final List<Exception> errors = new ArrayList<Exception>();
+
+        // short-circuit if a valid course couldn't be found
+        final Course course = this.getCourse(courseQuery.clone().loadManifest(true).loadPendingManifest(true));
+        if (course == null) {
+            // TODO: maybe throw an exception instead?
+            return null;
+        }
+
+        // don't do anything if there isn't a pending manifest
+        final String pendingManifest = course.getPendingManifest();
+        if (course.getPendingManifest() == null) {
+            // TODO: maybe throw an exception instead?
+            return course;
+        }
+
+        // validate pending manifest
+        final Document manifest = DomUtils.parse(pendingManifest);
+
+        // make sure we have all the resources listed in the manifest
+        final Map<String, Resource> resources = new HashMap<String, Resource>();
+        for (final Element resourceNode : DomUtils.getElements(manifest, "/ekko:course/ekko:resources/ekko:resource")) {
+            final Resource resource = course.getResource(resourceNode.getAttribute("sha1"));
+            resources.put(resourceNode.getAttribute("id"), resource);
+        }
+
+        // TODO: finish manifest validation
+
+        // throw any errors we encountered
+
+        // reset published state of all resources
+        // XXX OPTIMIZATION: reset all flags with single update query
+        for (final Resource resource : course.getResources()) {
+            resource.setPublished(false);
+            resource.setMetaResource(false);
+        }
+
+        // TODO: mark published resources
+
+        // TODO: mark meta resources
+
+        // replace the manifest with the pending manifest
+        course.setManifest(pendingManifest);
+        course.setPendingManifest(null);
+
+        // remove any previously generated zip file
+        course.setZip(null);
+
+        // increment the version of the course
+        course.incVersion();
+
+        // return the updated course
+        return course;
+    }
+
+    @Transactional
     @Override
     public Course getCourse(final Long id) {
         return this.em.find(Course.class, id);
+    }
+
+    @Transactional
+    @Override
+    public Course getCourse(final CourseQuery query) {
+        final List<Course> courses = this.getCourses(query);
+        if (courses.size() > 0) {
+            return courses.get(0);
+        }
+
+        return null;
+    }
+
+    @Transactional
+    @Override
+    public List<Course> getCourses(final CourseQuery query) {
+        // compile, execute & return the results for this query
+        return query.compile(this.em).getResultList();
+    }
+
+    @Transactional
+    @Override
+    public Course updateCourseAdmins(final CourseQuery query, final Collection<String> toAdd,
+            final Collection<String> toRemove) {
+        // short-circuit if a valid course couldn't be found
+        final Course course = this.getCourse(query.clone());
+        if (course == null) {
+            // TODO: maybe throw an exception instead?
+            return null;
+        }
+
+        // remove admins, then add new admins
+        course.removeAdmins(toRemove);
+        course.addAdmins(toAdd);
+
+        // return the updated course
+        return course;
+    }
+
+    @Transactional
+    @Override
+    public Course updateCourseEnrolled(final CourseQuery query, final Collection<String> toAdd,
+            final Collection<String> toRemove) {
+        // short-circuit if a valid course couldn't be found
+        final Course course = this.getCourse(query.clone());
+        if (course == null) {
+            // TODO: maybe throw an exception instead?
+            return null;
+        }
+
+        // remove enrolled, then add new enrolled
+        course.removeEnrolled(toRemove);
+        course.addEnrolled(toAdd);
+
+        // return the updated course
+        return course;
+    }
+
+    @Transactional
+    @Override
+    public Resource getResource(final ResourcePrimaryKey key) {
+        return this.em.find(Resource.class, key);
+    }
+
+    @Transactional
+    @Override
+    public Resource storeCourseZip(final Course course, final Resource resource) {
+        final Course dbCourse = this.em.find(Course.class, course.getId());
+
+        if (dbCourse.getZipSha1() == null) {
+            this.em.persist(resource);
+            dbCourse.setZip(resource);
+            return resource;
+        }
+
+        return null;
+    }
+
+    @Transactional
+    @Override
+    public Resource storeResource(final Course course, final Resource resource) {
+        this.em.persist(resource);
+        return resource;
+    }
+
+    @Transactional
+    @Override
+    public void removeResource(final Resource resource) {
+        this.em.remove(this.em.merge(resource));
     }
 }
